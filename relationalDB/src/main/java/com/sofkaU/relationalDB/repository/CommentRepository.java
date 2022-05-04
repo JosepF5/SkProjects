89c155c5c77561313222b18ffa795bc5ca6d8ecc
Josep Palomino <jfpalomino.22@gmail.com>
2022-05-04 08:07:26 -0500

    CREACION REPOSITORY
    se implemento la interface post y comment

---
+++ relationalDB/src/main/java/com/sofkaU/relationalDB/repository/CommentRepository.java
@@ -0,0 +1,8 @@
+package com.sofkaU.relationalDB.repository;
+
+
+import com.sofkaU.relationalDB.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository  extends JpaRepository<Comment,Long> {
+}
