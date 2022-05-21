a2579720517ef86383185299bba85ced5d23561a
Josep Palomino <jfpalomino.22@gmail.com>
2022-05-20 20:15:59 -0500

    Update Sale.Sale v.1.1

--- challengeDDD/src/main/java/co/com/sofka/challengeDDD/domain/food/Food.java
+++ challengeDDD/src/main/java/co/com/sofka/challengeDDD/domain/food/Food.java
@@ -1,12 +1,12 @@
 package co.com.sofka.challengeDDD.domain.food;
 
-import co.com.sofka.challengeDDD.domain.food.events.FoodCreated;
-import co.com.sofka.challengeDDD.domain.food.values.Payment;
-import co.com.sofka.challengeDDD.domain.food.values.FoodID;
+import co.com.sofka.challengeDDD.domain.food.events.*;
+import co.com.sofka.challengeDDD.domain.food.values.*;
 import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.domain.generic.DomainEvent;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class Food extends AggregateEvent<FoodID> {
@@ -29,6 +29,89 @@ public class Food extends AggregateEvent<FoodID> {
         var food = new Food(foodID);
         events.forEach(food::applyEvent);
         return food;
+    }
+
+    public void updatePayment(Payment payment){
+        Objects.requireNonNull(payment);
+        appendChange(new PaymentUpdated(payment)).apply();
+    }
+
+    public void addAppetizer(AppetizerID appetizerID, Name name, Price price, Texture texture){
+        Objects.requireNonNull(appetizerID);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(price);
+        Objects.requireNonNull(texture);
+        appendChange(new AppetizerAdded( appetizerID,  name,  price,  texture)).apply();
+    }
+
+    public void addDessert(DessertID dessertID, Name name, Price price, Taste taste){
+        Objects.requireNonNull(dessertID);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(price);
+        Objects.requireNonNull(taste);
+        appendChange(new DessertAdded( dessertID,  name,  price,  taste)).apply();
+    }
+
+    public void addDrink(DrinkID drinkID, Name name, Price price, Size size){
+        Objects.requireNonNull(drinkID);
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(price);
+        Objects.requireNonNull(size);
+        appendChange(new DrinkAdded( drinkID,  name,  price,  size)).apply();
+    }
+
+    public void changeAppetizerTexture(AppetizerID appetizerID, Texture texture){
+        Objects.requireNonNull(appetizerID);
+        Objects.requireNonNull(texture);
+        appendChange(new AppertizerTextureChanged( appetizerID,  texture)).apply();
+    }
+
+    public void changeDrinkSize(DrinkID drinkID, Size size){
+        Objects.requireNonNull(drinkID);
+        Objects.requireNonNull(size);
+        appendChange(new DrinkSizeChanged( drinkID,  size)).apply();
+    }
+
+    public void updateAppetizerName(AppetizerID appetizerID, Name name){
+        Objects.requireNonNull(appetizerID);
+        Objects.requireNonNull(name);
+        appendChange(new AppetizerNameUpdated( appetizerID,  name)).apply();
+    }
+
+    public void updateAppetizerPrice(AppetizerID appetizerID ,Price price){
+        Objects.requireNonNull(appetizerID);
+        Objects.requireNonNull(price);
+        appendChange(new AppetizerPriceUpdated( appetizerID , price)).apply();
+    }
+
+    public void updateDessertName(DessertID dessertID, Name name){
+        Objects.requireNonNull(dessertID);
+        Objects.requireNonNull(name);
+        appendChange(new DessertNameUpdated( dessertID,  name)).apply();
+    }
+
+    public void updateDessertPrice(DessertID dessertID, Price price){
+        Objects.requireNonNull(dessertID);
+        Objects.requireNonNull(price);
+        appendChange(new DessertPriceUpdated( dessertID,  price)).apply();
+    }
+
+    public void updateDessertTaste(DessertID dessertID, Taste taste){
+        Objects.requireNonNull(dessertID);
+        Objects.requireNonNull(taste);
+        appendChange(new DessertTasteUpdated( dessertID,  taste)).apply();
+    }
+
+    public void updateDrinkName(DrinkID drinkID, Name name){
+        Objects.requireNonNull(drinkID);
+        Objects.requireNonNull(name);
+        appendChange(new DrinkNameUpdated( drinkID,  name)).apply();
+    }
+
+    public void updateDrinkPrice(DrinkID drinkID,Price price){
+        Objects.requireNonNull(drinkID);
+        Objects.requireNonNull(price);
+        appendChange(new DrinkPriceUpdated( drinkID, price)).apply();
     }
 
     public Payment payment() {
