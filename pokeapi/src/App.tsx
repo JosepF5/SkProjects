73b80fcd7d94e4d2a17fedd01c959bef1e1fab17
Josep Palomino <jfpalomino.22@gmail.com>
2022-07-14 17:46:19 -0500

    PokeApi v2.0

--- pokeapi/src/App.tsx
+++ pokeapi/src/App.tsx
@@ -2,36 +2,38 @@ import { useEffect, useState } from "react";
 import './App.css'
 import { useSelector, useDispatch } from "react-redux";
 import { stateType, AppDispatch } from "../src/state/store";
-import {
-  getAllPokemons,
-  selectPokemonStatus,
-  selectPokemon,
-  selectPokemonError,
-  pokemonFetchStatus
-} from "../src/state/features/pokemonSlice";
-
+import {getPokemons} from "../src/state/features/pokemonSlice";
+import Pokemon from "../src/state/features/components/Pokemon"
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-  const pokemonState = useSelector(selectPokemon());
-  const pokemonStatus = useSelector(selectPokemonStatus());
-  const pokemonError = useSelector(selectPokemonError());
-
-  useEffect(() => {
-    console.log("hola effect")
-    if (pokemonStatus === pokemonFetchStatus.IDLE) {
-      console.log("hola effect")
-      dispatch(getAllPokemons());
+  const dispatch = useDispatch();
+  const pokemons=useSelector(
+    (state:stateType)=>state.pokemons
+  )
+  const [loading,setLoading]=useState(true);
+  const getEveryPokemon = async (id:number)=>{
+    let data: any[]=[]
+    for (let ide = 1; ide <= id; ide++) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${ide}/`)
+    data = [...data, await response.json()]
     }
-  },[dispatch]);
-  
-  console.log(pokemonState.results)
+    return data
+  }
+  useEffect(() => {
+    getEveryPokemon(50).then((res)=>{
+        dispatch(getPokemons(res));
+        setLoading(false)
+    });
+    
+  },[]);
+  //console.log(pokemons)
   return (
-    <div className="App">
-      {/*pokemonState.results.map((pokemon:any) => {
+    <div>
+      {!loading&&pokemons.map((pokemon:any) => {
           return (
-            <p>{pokemon.name}</p>
+            <Pokemon key={pokemon.name} pokemon={pokemon}/>
           );
-        })*/}
+        })
+      }
     </div>
   )
 }
