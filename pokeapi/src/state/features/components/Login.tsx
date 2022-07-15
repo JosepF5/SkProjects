aed0da1918644d063efe79592182b986bba4b2c1
Josep Palomino <jfpalomino.22@gmail.com>
2022-07-15 00:32:19 -0500

    Login 1.0

--- pokeapi/src/state/features/components/Login.tsx
+++ pokeapi/src/state/features/components/Login.tsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../../../firebase/firebaseConfig";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logInInReducer } from "../../features/loggedInSlice";
+import Button from "react-bootstrap/Button";
 
-const Login = () => {
+const LogIn: React.FunctionComponent = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const logInForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (userName && password) {
+      signInWithEmailAndPassword(auth, userName, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          dispatch(logInInReducer(user));
+          navigate("/welcome");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+        });
+      setPassword("");
+      setUserName("");
+    }
+  };
   return (
     <div>
-      Hola
+      <h1>Log In</h1>
+      <form>
+        <label htmlFor="username">Username</label>
+        <br />
+        <input
+          onChange={(e) => setUserName(e.target.value)}
+          type="text"
+          name="username"
+          value={userName}
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          name="password"
+          value={password}
+        />
+        <br />
+        <br />
+        <Button variant="primary" onClick={(e) => logInForm(e)}>
+        Log In
+        </Button>
+        <br />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default LogIn;
